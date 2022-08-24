c3c3b9ad666ba91b41bd790b4b844a7ba9f89eeb
Terbeche Mustapha <termus96@gmail.com>
2022-08-24 14:06:16 +0100

    Create many-to-many relaionship between Person and Book using Rental

--- rental.rb
+++ rental.rb
@@ -1,7 +1,13 @@
 class Rental
   attr_accessor :data
+  attr_reader :person, :book
 
-  def initialize(data)
+  def initialize(data, person, book)
     @data = data
+
+    @person = person
+    person.rentals << self
+    @book = book
+    book.rentals << self
   end
 end
